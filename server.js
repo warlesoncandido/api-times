f9bb1e0b51eb62589a3906af190f25d74be53c57
Warleson Candido <warleson.silva@evoluum.com.br>
2022-04-20 17:27:25 -0300

    Change port

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
+const port = process.env.PORT || 3000;
 
 
 const estadosRouters = require("./routers/estados");
 const timesRouters = require("./routers/times");
 app.use(cors());
 
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
 app.use('/estados',estadosRouters);
 app.use('/times',timesRouters);
 
-app.listen(3000,()=>{console.log('Server running')});
+app.listen(port,()=>{console.log(`Server running in ${port}`)});
