b1c8c5730fea79e8cbfeace81d62d7b3b269b95d
Warleson Candido <warleson.silva@evoluum.com.br>
2022-04-21 20:23:27 -0300

    conectando a banco online

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
-(async()=>{const express = require('express');
-const db = require('./db');
-const bodyParser = require('body-parser');
+(async () => {
+    require('dotenv').config();
+    const express = require('express');
+    const db = require('./db');
+    const bodyParser = require('body-parser');
 
-const app = express();
-const cors = require('cors')
-const port = process.env.PORT || 3000;
+    const app = express();
+    const cors = require('cors')
+    const port = process.env.PORT || 3000;
 
-const stateRouters = require("./routers/states");
-const teamsRouters = require("./routers/teams");
+    const stateRouters = require("./routers/states");
+    const teamsRouters = require("./routers/teams");
 
 
-app.use(bodyParser.urlencoded({
-    extended: true
-  }));
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
 
-app.use(bodyParser.json());
-app.use(cors());
+    app.use(bodyParser.json());
+    app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-})
+    app.get("/", (req, res) => {
+        res.send("Hello World");
+    })
 
-app.use('/estados',stateRouters);
-app.use('/times',teamsRouters);
+    app.use('/estados', stateRouters);
+    app.use('/times', teamsRouters);
 
-app.listen(port,()=>{console.log(`Server running in ${port}`)});})();
+    app.listen(port, () => { console.log(`Server running in ${port}`) });
+})();
